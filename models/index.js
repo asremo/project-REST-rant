95390cceea914ecbce9a24afa30813b4561ded06
Amanda Sharmaine Remo <asremo@amandas-mbp.lan>
2022-08-25 20:45:35 -0400

    connected to mongoose, created schema, edited the route page

---
+++ models/index.js
@@ -0,0 +1,10 @@
+//where we'll connect to Mongoose
+
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true, 
+  useUnifiedTopology: true
+})
+
+module.exports.Place = require('./places')
