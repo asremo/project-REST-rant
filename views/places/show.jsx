4d3ebf8ed9d5efb1f7b44dea74bdc6e5c204dc68
Amanda Sharmaine Remo <asremo@amandas-mbp.lan>
2022-08-30 21:16:25 -0400

    added comments into show page

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,7 +1,27 @@
 const React = require('react')
 const Def = require('../default')
 
+
 function show (data) {
+    let comments = (
+        <h3 className='inactive'>
+            No comments yet!
+        </h3>
+    )
+    if (data.place.comments.length) {
+        comments = data.place.comments.map(c => {
+            return (
+                <div className='border'>
+                    <h2 className='rant'>{c.rant ? 'Rant!' : 'Rave!'} </h2>
+                    <h4>{c.content}</h4>
+                    <h3>
+                        <strong>- {c.author}</strong>
+                    </h3>
+                    <h4>Rating: {c.stars}</h4>
+                </div>
+            )
+        })
+    }
     return (
         <Def>
             <main>
@@ -19,8 +39,6 @@ function show (data) {
                         <h2>Description</h2>
                         <h3>{data.place.showEstablished()}</h3>
                         <h4>Serving {data.place.cuisines}</h4>
-                        <h2>Comments</h2>
-                        <h3>No comments yet</h3>
                         <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
                             Edit
                         </a>
@@ -31,6 +49,9 @@ function show (data) {
                         </form> 
                     </div>
                 </div>
+                <hr />
+                <h2>Comments</h2>
+                {comments}
           </main>
         </Def>
     )
